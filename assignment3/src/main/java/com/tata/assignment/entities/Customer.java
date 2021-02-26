4f77bfaccf6b503e06007f8476a3be902cc0e974
Yashas <47943716+yashas56@users.noreply.github.com>
2021-02-26 11:52:48 +0530

    Delete assignment3 directory

--- assignment3/src/main/java/com/tata/assignment/entities/Customer.java
+++
@@ -1,7 +0,0 @@
-package com.tata.assignment.entities;
-
-public class Customer {
-    private String address,email,name,password,secretAns,secretQues;
-    private long phoneNo;
-    private int userId;
-}
